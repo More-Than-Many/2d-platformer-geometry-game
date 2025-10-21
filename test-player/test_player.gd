45cf1412d0c8ffd2a3d6a44432f5938742c485eb
UltraKingLoser <jeremystanton97@gmail.com>
2025-10-21 17:28:51 +1000

    movement fix
    player wont spin off when they hold down input

---
+++ test-player/test_player.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+@export var torque_force = 8000
+
+func _ready():
+	print(torque_force)
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		apply_torque(torque_force)
+	
+	if Input.is_action_pressed("right"):
+		apply_torque(-torque_force)
+		
+	angular_velocity = clamp(angular_velocity, -PI, PI)
