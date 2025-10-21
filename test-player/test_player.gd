110b6b644de0e9e76d1a102b25e9f152054703ea
UltraKingLoser <jeremystanton97@gmail.com>
2025-10-21 22:02:51 +1000

    torque switch
    torque switch

--- test-player/test_player.gd
+++ test-player/test_player.gd
@@ -7,9 +7,9 @@ func _ready():
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("left"):
-		apply_torque(torque_force)
+		apply_torque(-torque_force)
 	
 	if Input.is_action_pressed("right"):
-		apply_torque(-torque_force)
+		apply_torque(torque_force)
 		
 	angular_velocity = clamp(angular_velocity, -PI, PI)
