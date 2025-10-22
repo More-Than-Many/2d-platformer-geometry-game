d09f48013cef33086b9995bacad2ec2499d5994d
Sean <89282852+seanevaa@users.noreply.github.com>
2025-10-22 08:53:31 -0700

    Reformatting

--- test-player/test_player.gd
+++ test-player/test_player.gd
@@ -3,7 +3,25 @@ extends RigidBody2D
 @export var base_torque_force := 12000.0
 @export var jump_height := 1200.0
 
+@export_group("Nodes"
 @export var CornerHolder : Node
+
+@export_group("Input")
+@export var input_left := "move_left"
+@export var input_right := "move_right"
+@export var input_jump := "move_jump"
+
+@export_group("Player Movement")
+@export var base_torque := 12000.0
+@export var jump_height := 1200.0
+
+@export_group("Player Abilities")
+@export var frozen := false
+@export var can_move := true
+@export var can_jump := true
+
+var direction : float
+var recent_direction : float
 
 var corners = []
 
@@ -17,10 +35,16 @@ func _ready():
 			corners.append(child)
 
 func _physics_process(_delta: float) -> void:
+	if not can_move:
+		return
+
 	input_direction = Input.get_axis("left", "right")
 	current_torque_force = lerp(current_torque_force, input_direction * base_torque_force, 0.5) #Upon further inspection this may be a bad way of doing this, will try a different method of lerping
 	
 	apply_torque(current_torque_force)
+
+	if not can_jump:
+		return
 	
 	if Input.is_action_just_pressed("jump"):
 		jump()
