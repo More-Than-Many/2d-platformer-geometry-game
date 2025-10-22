1c16339b0d0ae1831eb86439dbadf1fb510ebba6
Sean <89282852+SodumbX@users.noreply.github.com>
2025-10-21 18:54:40 -0700

    Speed Adjusted

--- test-player/test_player.gd
+++ test-player/test_player.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-@export var base_torque_force := 16000.0
+@export var base_torque_force := 12000.0
 
 var input_direction : float
 
@@ -11,9 +11,8 @@ func _ready():
 
 func _physics_process(_delta: float) -> void:
 	input_direction = Input.get_axis("left", "right")
-	current_torque_force = lerp(current_torque_force, input_direction * base_torque_force, 0.3)
+	current_torque_force = lerp(current_torque_force, input_direction * base_torque_force, 0.5)
 	
 	apply_torque(current_torque_force)
-	print(current_torque_force)
 		
 	angular_velocity = clamp(angular_velocity, -PI, PI)
