ab2a1ad60df0ef395cc7b1680094fd3eeb66b19d
Sean <89282852+SodumbX@users.noreply.github.com>
2025-10-22 19:48:15 -0700

    Very Unfinished Gravity

--- test-player/test_player.gd
+++ test-player/test_player.gd
@@ -12,6 +12,11 @@ extends RigidBody2D
 @export var base_torque := 12000.0
 @export var jump_height := 1200.0
 @export var jump_length := 600.0
+@export var input_direction_pull := 0.5
+
+@export_group("Gravity")
+@export var gravity_strength := 980.0
+@export var gravity_direction := Vector2.DOWN
 
 @export_group("Player Abilities")
 @export var frozen := false
@@ -32,23 +37,25 @@ func _physics_process(_delta: float) -> void:
 	if not can_move:
 		return
 
-	input_direction = Input.get_axis("left", "right")
-	current_torque = lerp(current_torque, input_direction * base_torque, 0.5)
+	input_direction = Input.get_axis(input_left, input_right)
+	current_torque = lerp(current_torque, input_direction * base_torque, input_direction_pull)
 	apply_torque(current_torque)
 
 	if not can_jump:
 		return
 	
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed(input_jump):
 		jump()
 		
 	angular_velocity = clamp(angular_velocity, -PI, PI)
+
+func _integrate_forces(state):
+	pass
 
 
 func jump() -> void:
 	var highest_corner_position = Vector2(Center.global_position.x, Center.global_position.y - 32)
 	
-
 	for corner in corners:
 		if abs(corner.global_position.y - (Center.global_position.y - 32)) < 1.0:
 			pass
