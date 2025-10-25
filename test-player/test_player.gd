3f2469ed0ba7b146c63eb3aead6182a4a415a09c
Sean <89282852+SodumbX@users.noreply.github.com>
2025-10-25 01:49:30 -0700

    Update test_player.gd

--- test-player/test_player.gd
+++ test-player/test_player.gd
@@ -65,13 +65,7 @@ func jump() -> void:
 	if not can_jump:
 		return
 	
-	gravity_change(Vector2.DOWN)
-	
 	var gravity_angle := gravity_direction.angle() - deg_to_rad(90)
-	print(gravity_angle)
-	
-	var test = Vector2.DOWN.rotated(gravity_angle)
-	print(test)
 	
 	var highest_corner_position = Vector2(Center.global_position.x, Center.global_position.y - 32)
 	
@@ -85,6 +79,8 @@ func jump() -> void:
 	
 	linear_velocity.x = direction_to_corner.x * jump_length
 	linear_velocity.y = direction_to_corner.y * jump_height
+	
+	gravity_change(Vector2.DOWN)
 
 func stick() -> void:
 	for raycast in raycasts:
